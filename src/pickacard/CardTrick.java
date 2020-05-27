f91db1d37254058dd87f631cc865b0b3fd8b244d
Saad740-s <saadalmurad@gmail.com>
2020-05-26 23:36:09 -0400

    Exercise 1

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,27 +1,33 @@
 package pickacard;
-
+import java.util.Random;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
  * @author dancye
  * @author Paul Bonenfant
+ * @author Saad Almorad
  */
-public class CardTrick {
+public class CardTrick
+{
 
-    public static void main(String[] args) {
-        
-        Card[] magicHand = new Card[7];
+   public static void main (String[] args)
+   {
 
-        for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
+      Card[] magicHand = new Card[7];
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
+      for (int i = 0; i < magicHand.length; i++) {
+         Card c = new Card();
+         //c.setValue(insert call to random number generator here)
+         Random rnd = new Random();
+         c.setValue(rnd.nextInt(13) + 1);
+         //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+         c.setSuit(Card.SUITS[rnd.nextInt() + 3]);
+      }
+
+      //insert code to ask the user for Card value and suit, create their card
+      // and search magicHand here
+      //Then report the result here
+   }
 
 }
