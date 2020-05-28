2b8e3aab37f27b3240c1705d8eb9a2b2b0cd5b22
Saad740-s <saadalmurad@gmail.com>
2020-05-27 23:31:06 -0400

    Exercise 1

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,5 +1,6 @@
 package pickacard;
 import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
@@ -22,12 +23,44 @@ public class CardTrick
          Random rnd = new Random();
          c.setValue(rnd.nextInt(13) + 1);
          //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-         c.setSuit(Card.SUITS[rnd.nextInt() + 3]);
+         c.setSuit(Card.SUITS[rnd.nextInt(3)]);
+         System.out.println(c.getValue());
+         System.out.println(c.getSuit());
       }
 
       //insert code to ask the user for Card value and suit, create their card
       // and search magicHand here
-      //Then report the result here
-   }
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Pick a Card! (Hearts, Diamonds, Spades, Clubs)");
+      String suit = scan.nextLine();
+      System.out.println("Pick a number (1-13)");
+      int value = scan.nextInt();
 
+
+
+
+      Card magicCard = new Card();
+      magicCard.setValue(7);
+      magicCard.setSuit(Card.SUITS[1]);
+      suit = magicCard.getSuit();
+      value = magicCard.getValue();
+
+      for (int i = 0; i < magicHand.length; i++) {
+         if (suit.equals(magicHand[i].getSuit())) {
+            if (value == magicHand[i].getValue()) {
+               System.out.println("Congrats.. you win!!");
+               break;
+            }
+            else if (magicHand.length - 1 == i) {
+               System.out.println("You lost... try agian!");
+            }
+            else {
+
+            }
+         }
+
+//      Then report the result here
+      }
+
+   }
 }
