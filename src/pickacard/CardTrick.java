37c02e3319dd0af0d4a4398350c3a20952974a3e
AC <AC@LAPTOP-ACCNGVK5>
2020-05-27 23:31:08 -0400

    PickACard file

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,27 +1,41 @@
 package pickacard;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
- * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
- *
- * @author dancye
- * @author Paul Bonenfant
+ * @author Aryan Chopra
  */
-public class CardTrick {
+public class CardTrick
+{
+   public static void main (String[] args)
+   {
+      Card[] magicHand = new Card[7];
+      for (int i = 0; i < magicHand.length; i++) {
+         Card c = new Card();
+         Random rnd = new Random();
+         c.setValue(rnd.nextInt(13) + 1);
+         c.setSuit(Card.SUITS[rnd.nextInt(4)]);
+         magicHand[i] = c;
+      }
 
-    public static void main(String[] args) {
-        
-        Card[] magicHand = new Card[7];
 
-        for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Enter the value of Card");
+      int value = scan.nextInt();
+      System.out.println("Enter the Suit value");
+      String suit = scan.next();
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
 
+      String result = "";
+      for (int j = 0; j < magicHand.length; j++) {
+         System.out.println(magicHand[j].getValue() + magicHand[j].getSuit());
+         if (value == magicHand[j].getValue() && suit.equalsIgnoreCase(magicHand[j].getSuit())) {
+            result = "Magic card is in your hand";
+            break;
+         }
+         else {
+            result = "Better luck next time...";
+         }
+      }
+      System.out.println(result);
+   }
 }
