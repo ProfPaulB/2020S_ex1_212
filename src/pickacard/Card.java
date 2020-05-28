4002a599876330449b0124e951e858acc35c1080
Elie Khuzam <khuzam@sheridancollege.ca>
2020-05-27 22:04:31 -0400

    added a new Card Object called luckyCard

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -11,12 +11,19 @@ package pickacard;
  * @author dancye
  * @author Paul Bonenfant
  * @author Elie Khuzam
+ *
  */
 public class Card
 {
 
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
+
+   public Card (String suit, int value)
+   {
+      this.suit = suit;
+      this.value = value;
+   }
 
    public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
 
--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,27 +1,61 @@
 package pickacard;
-
+import java.util.Random;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
  * @author dancye
  * @author Paul Bonenfant
+ * @author Elie Khuzam
+ * date: 27/05/2020
  */
-public class CardTrick {
+public class CardTrick
+{
 
-    public static void main(String[] args) {
-        
-        Card[] magicHand = new Card[7];
+   public static void main (String[] args)
+   {
+      Card[] magicHand = new Card[7];
+      Random rnd = new Random();
 
-        for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
+      for (int i = 0; i < magicHand.length; i++) {
+         int value = rnd.nextInt(13) + 1;
+         int rank = rnd.nextInt(3);
+         magicHand[i] = new Card(Card.SUITS[rank], value);
+      }
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
+      int newValue = 7;
+      String newSuit = "Diamonds";
 
+      Card luckyCard = new Card(newSuit, newValue);
+
+
+      for (int i = 0; i < magicHand.length; i++) {
+         if (newValue == luckyCard.getValue() && newSuit.equals(luckyCard.getSuit())) {
+            System.out.println("You Win!");
+            break;
+         }
+         System.out.println("Try Again");
+      }
+
+
+//      Scanner scan = new Scanner(System.in);
+//      System.out.println("Pick a Card!!");
+//      int value = scan.nextInt();
+//      String suit = scan.nextLine();
+
+//      String magicCard = "";
+//      for (int i = 0; i < magicHand.length; i++) {
+//         if (c.getValue() == value && c.getSuit().equals(suit)) {
+//            magicCard = "You Win!!";
+//            break;
+//         }
+//         magicCard = "Try Again!!";
+//      }
+//      System.out.println(magicCard);
+
+
+//insert code to ask the user for Card value and suit, create their card
+      // and search magicHand here
+      //Then report the result here
+   }
 }
