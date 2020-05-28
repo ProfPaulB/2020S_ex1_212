96f1cffa50d8b111d6dee50ddf7154028059cc80
jdgarzonl <garzonle@sheridancollege.ca>
2020-05-27 23:04:50 -0400

    1. Second version without any modifications in the Card class (not clear in assignment instructions if the Card class could be modified). 2. Debugging lines to printout of the magic hand are commented.

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
  *
  * @author dancye
  * @author Paul Bonenfant
- * @author Juan Garzon 2020/05/27
+ * @author Juan Garzon 
+ * Date: 2020/05/27
  */
 public class CardTrick {
 
@@ -21,29 +22,24 @@ public class CardTrick {
             
             int randomValue = (int) (1 + 13 * Math.random());
             int randomSuit = (int) (4 * Math.random());
-            
-//            System.out.println("Card #" + i);
-//            System.out.println("Value " + value);
-//            System.out.println("Suit " + suit);
-//            System.out.println();
-            
+         
             //c.setValue(insert call to random number generator here)
             c.setValue(randomValue);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             c.setSuit(Card.SUITS[randomSuit]);
             magicHand[i] = c;
         }
-
-        for (Card card: magicHand)
-        {
-            System.out.printf("%d of %s %n", card.getValue(), card.getSuit());
-        }
+        //The magic hand is printed out for debugging purposes
+//        for (Card card: magicHand)
+//        {
+//            System.out.printf("%d of %s %n", card.getValue(), card.getSuit());
+//        }
         
         //insert code to ask the user for Card value and suit, create their card
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the value [1 to 13]");
+        System.out.println("Please enter the card's value [1 to 13]");
         int pickedValue = Integer.parseInt(input.nextLine());
-        System.out.println("Please enter the suit [0 for Hearts " +
+        System.out.println("Please enter the card's suit [0 for Hearts, " +
             "1 for Diamonds, 2 for Spades, 3 for Clubs]");
         int pickedSuit = Integer.parseInt(input.nextLine());
         
@@ -51,19 +47,19 @@ public class CardTrick {
         pickedCard.setValue(pickedValue);
         pickedCard.setSuit(Card.SUITS[pickedSuit]);
         
-        System.out.printf("You have selected the %d of %s %n", pickedCard.getValue(), pickedCard.getSuit());
+        System.out.printf("You have selected the [%d of %s]%n", 
+            pickedCard.getValue(), pickedCard.getSuit());
         // and search magicHand here
         boolean isFound = false;
         for (Card card: magicHand)
         {            
-            if(card.equals(pickedCard))
+            if (pickedCard.getValue() == card.getValue() && pickedCard.getSuit() == card.getSuit())
             {
                 isFound = true;
             }
         }
         
-        //Then report the result here
-                
+        //Then report the result here                
         if(isFound)
         {
             System.out.println("Congratulations your card is in the magic hand!!!");
